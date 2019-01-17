44e3dfef44495d2e87aaffa2cd21694b7695ac71
kimzing <kimzing@163.com>
2019-01-17 21:28:00 +0800

    初始化网关项目

---
+++ gateway/src/main/java/com/kimzing/gateway/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.kimzing.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Gateway启动程序
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
